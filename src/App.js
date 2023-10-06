5f0552c6b6d07bdd5de2f0f86c5f72b158141c91
Eoghanfox34 <G00400704@atu.ie>
2023-10-06 14:03:59 +0000

    Lab2

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import Content from './Components/content';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+
+    </div>
+  );
+}
+
+export default App;
