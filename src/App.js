864b0cdc9135bcd3fd5772d2fd5570640254bb3c
Eoghanfox34 <G00400704@atu.ie>
2023-10-06 14:28:35 +0000

    Lab2 no comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/header';
 import Footer from './Components/footer';
 import Content from './Components/content';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {
+  BrowserRouter, Routes, Route} from "react-router-dom";
+
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Content></Content>}></Route>
+        <Route path='/read' element={<Header></Header>}></Route>
+        <Route path='/create' element={<Footer></Footer>}></Route>
+        </Routes>     
+
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
-
     </div>
+    </BrowserRouter>
   );
 }
 
